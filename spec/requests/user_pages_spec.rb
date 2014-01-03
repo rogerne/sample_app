6410710cf2c9c63265c5729e151794590393d4b3
Neil Rogers <rogerne@btinternet.com>
2014-01-03 05:33:05 +0000

    Complete user sign up

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -26,14 +26,23 @@ describe "User pages" do
     let(:submit) { "Create my account" }
 
     describe "with invalid information" do
-      
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
+
+      describe "after submission" do  
+        before { click_button submit }
+        it { should have_selector('title', text: 'Sign up') }
+        it { should have_content('Password digest can\'t be blank') }
+        it { should have_content('Name can\'t be blank') }
+        it { should have_content('Email can\'t be blank') }
+        it { should have_content('Email is invalid') }
+        it { should have_content('Password can\'t be blank') }
+        it { should have_content('Password is too short (minimum is') }
+      end
     end
 
-    describe "with valis information" do
-  
+    describe "with valid information" do
       before do
         fill_in "Name",          with: "Example User"
         fill_in "Email",         with: "user@example.com"
@@ -44,6 +53,11 @@ describe "User pages" do
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
       end
-    end  
-  end 
+
+      describe "after saving the user" do
+        before { click_button submit }
+        it { should have_selector('title', text: 'Example User') }
+      end
+    end
+  end
 end
