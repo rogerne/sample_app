61d1271d0405c53fdbab949084b76c0dbad67db2
Neil Rogers <rogerne@btinternet.com>
2014-01-03 07:20:24 +0000

    Add better message for missing password

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,12 +21,14 @@ class User < ActiveRecord::Base
   			  			length: {maximum: 50}
 
   	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
   	validates :email, 	presence: true, 
   						format: { with: VALID_EMAIL_REGEX },
   						uniqueness: { case_sensitive: false}
-
-  	validates :password, presence: true,
-  						 length: {minimum: 6}
+    #Todo Excercise 7.6.2 confirm validity of removing presence: true
+    validates :password, length: {minimum: 6} 	
+    #validates :password, presence: true,
+  	#					 length: {minimum: 6}
 
   	validates :password_confirmation, presence: true 	
 
