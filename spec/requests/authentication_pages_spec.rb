09017198859ef08128c91620dbe4eb017b609311
Neil Rogers <rogerne@btinternet.com>
2014-01-06 10:55:12 +0000

    before_filter implemented for edit and update

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -43,6 +43,7 @@ describe "Authentication" do
       #   fill_in "Password", with: user.password
       #   click_button "Sign in"
       # end
+
       before { sign_in user }
 
       it { should have_selector('title', text: user.name) }
@@ -51,5 +52,26 @@ describe "Authentication" do
       it { should have_link('Sign out',    href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
     end
-  end    
+  end
+
+  describe "authorization" do
+
+    describe "for non-signed-in users" do
+      let(:user) { FactoryGirl.create(:user) }
+
+      describe "in the Users controller" do
+
+        describe "visiting the edit page" do
+          before { visit edit_user_path(user) }
+          it { should have_selector('title', text: 'Sign in') }
+        end
+
+        describe "submitting to the update action" do
+          before { put user_path(user) }
+          specify { response.should redirect_to(signin_path) }
+        end
+
+      end
+    end
+  end
 end
