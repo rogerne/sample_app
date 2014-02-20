85ed61774088e63adde21bd113c238eb5b465a5c
Neil Rogers <rogerne@btinternet.com>
2014-02-20 20:51:59 +0000

    Delete Microposts

--- spec/requests/micropost_pages_spec.rb
+++ spec/requests/micropost_pages_spec.rb
@@ -28,6 +28,18 @@ describe "Micropost pages" do
       it "should create a micropost" do
         expect { click_button "Post" }.should change(Micropost, :count).by(1)
       end
-    end
+    end    
   end
+
+  describe "micropost destruction" do
+    before { FactoryGirl.create(:micropost, user: user) }
+
+    describe "as correct user" do
+      before { visit root_path }
+
+      it "should delete a micropost" do
+        expect { click_link "delete" }.should change(Micropost, :count).by(-1)
+      end
+    end
+  end  
 end
